075b3c101eb9b3ef2f40e6b2300c0baf545d922a
Yosr Beydoun <ybaydoun@hotmail.com>
2021-08-12 18:03:17 +0300

    Frequency not saving

--- static/js/project.js
+++ static/js/project.js
@@ -6,24 +6,27 @@ $(document).ready(function() {
         $('#changelist-search').submit();
     });
 
-    $('.field-frequency').each(function (i, val) {
-        var item = $(this);
-        var input = item.find('input');
-        input.hide();
-        var frequency = parseInt(input.val());
+    if (isTrackerDetailsPage()) {
 
-        for (i = 0; i < frequency; i++) {
-            add_badge(item, i);
-        }
-    });
+        $('.field-frequency').each(function (i, val) {
+            var item = $(this);
+            var input = item.find('input');
+            input.hide();
+            var frequency = parseInt(input.val());
 
-    $('.field-frequency').dblclick(function (e) {
-        var item = $(this);
-        var input = item.find('input');
-        var frequency = parseInt(input.val());
-        input.val(frequency + 1);
-        add_badge(item, frequency);
-    });
+            for (i = 0; i < frequency; i++) {
+                add_badge(item, i);
+            }
+        });
+
+        $('.field-frequency').dblclick(function (e) {
+            var item = $(this);
+            var input = item.find('input');
+            var frequency = parseInt(input.val());
+            input.val(frequency + 1);
+            add_badge(item, frequency);
+        });
+    }
 
     $(document).on('click', '.close', function(){
         $("#feedbackModal").hide();
